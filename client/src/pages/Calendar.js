b61aa94ae7347ad48f268ff7d1610fcd06b75f4b
Johnny <117334322+JohnnyB90@users.noreply.github.com>
2023-06-29 21:43:22 -0400

    Setup calendar code and tied to app.js

---
+++ client/src/pages/Calendar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Calendar from "../components/Calendar";
+
+
+function handleCalendar() {
+
+    return (
+      <div>
+      {<Calendar />}
+      </div>
+    );
+  }
+  
+  export default handleCalendar;

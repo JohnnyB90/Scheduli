8850cdd2bf0e74cf90fb620c5874fb989ebfbbe9
Johnny <117334322+JohnnyB90@users.noreply.github.com>
2023-07-05 19:02:02 -0400

    Finished updates in userTypeDefs

---
+++ server/schemas/typeDefs/userTypeDefs.js
@@ -0,0 +1,48 @@
+const { gql } = require('apollo-server-express');
+
+const userTypeDefs = gql`
+  type User {
+    _id: ID
+    firstName: String!
+    lastName: String!
+    email: String!
+    password: String!
+    phoneNumber: String!
+    businessName: String!
+    businessAddress: String!
+    zipCode: String!
+    city: String!
+    state: String!
+    country: String!
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    user: User
+  }
+
+  type Mutation {
+    addUser(firstName: String!, lastName: String!, email: String!, password: String!, phoneNumber: String!, businessName: String!, businessAddress: String!, zipCode: String!,  city: String!, state: String!, country: String!): Auth
+    updateUser(
+      _id: ID!, 
+      email: String, 
+      password: String, 
+      phoneNumber: String,
+      businessName: String,
+      businessAddress: String,
+      zipCode: String,
+      city: String,
+      state: String,
+      country: String
+    ): User
+    deleteUser(_id: ID!): User
+    login(email: String!, password: String!): Auth
+  }
+`;
+
+module.exports = userTypeDefs;
+

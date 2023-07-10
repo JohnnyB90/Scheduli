ee45ebea812a8e3a21ff54810dabf33ec1c3a170
Matthew Baty <mattbaty@outlook.com>
2023-07-09 22:01:21 -0400

    account settings

--- client/src/components/PasswordSettings/index.js
+++ client/src/components/PasswordSettings/index.js
@@ -30,84 +30,85 @@ export default function PasswordSettings() {
         event.preventDefault();
 
         try { // use mutation
-        // Password change logic
-        // ...
+            // Password change logic
+            // ...
 
-        setSuccessMessage("Password successfully updated.");
-        setFormState({
-            currentPassword: "",
-            newPassword: "",
-            confirmPassword: "",
-        });
-    }
+            setSuccessMessage("Password successfully updated.");
+            setFormState({
+                currentPassword: "",
+                newPassword: "",
+                confirmPassword: "",
+            });
+        }
 
     return (
-        <section className="container">
-            <div className="row">
-                <div className="col-md-6 col-sm-12">
-                    <div className="card" id="settings-color">
-                        <div className="card-body p-5 account-settings-form">
-                            <h1 className="card-title text-Black text-center">Password Settings</h1>
-                            <form onSubmit={handleSubmit}>
-                                <div className="mb-3">
-                                    <label htmlFor="currentPassword" className="form-label text-black">
-                                        Current Password
-                                    </label>
-                                    <input
-                                        type="password"
-                                        className="form-control"
-                                        id="currentPassword"
-                                        name="currentPassword"
-                                        value={currentPassword}
-                                        onChange={handleChange}
-                                    />
-                                </div>
-                                <div className="mb-3">
-                                    <label htmlFor="newPassword" className="form-label text-black">
-                                        New Password
-                                    </label>
-                                    <input
-                                        type="password"
-                                        className="form-control"
-                                        id="newPassword"
-                                        name="newPassword"
-                                        value={newPassword}
-                                        onChange={handleChange}
-                                    />
-                                </div>
-                                <div className="mb-3">
-                                    <label htmlFor="confirmPassword" className="form-label text-black">
-                                        Confirm Password
-                                    </label>
-                                    <input
-                                        type="password"
-                                        className="form-control"
-                                        id="confirmPassword"
-                                        name="confirmPassword"
-                                        value={confirmPassword}
-                                        onChange={handleChange}
-                                    />
-                                </div>
-                                <div className="d-grid gap-2">
-                                    <button type="submit" className="btn btn-dark text-white">
-                                        Change Password
-                                    </button>
-                                </div>
-                            </form>
-                            {successMessage && (
-                                <div className="text-success mt-3 text-black text-bold text-center">
-                                    {successMessage}
-                                </div>
-                            )}
-                            {errorMessage && (
-                                <div className="text-danger mt-3 text-black text-bold text-center">
-                                    {errorMessage}
-                                </div>
-                            )}
+            <section className="container">
+                <div className="row">
+                    <div className="col-md-6 col-sm-12">
+                        <div className="card" id="settings-color">
+                            <div className="card-body p-5 account-settings-form">
+                                <h1 className="card-title text-Black text-center">Password Settings</h1>
+                                <form onSubmit={handleSubmit}>
+                                    <div className="mb-3">
+                                        <label htmlFor="currentPassword" className="form-label text-black">
+                                            Current Password
+                                        </label>
+                                        <input
+                                            type="password"
+                                            className="form-control"
+                                            id="currentPassword"
+                                            name="currentPassword"
+                                            value={currentPassword}
+                                            onChange={handleChange}
+                                        />
+                                    </div>
+                                    <div className="mb-3">
+                                        <label htmlFor="newPassword" className="form-label text-black">
+                                            New Password
+                                        </label>
+                                        <input
+                                            type="password"
+                                            className="form-control"
+                                            id="newPassword"
+                                            name="newPassword"
+                                            value={newPassword}
+                                            onChange={handleChange}
+                                        />
+                                    </div>
+                                    <div className="mb-3">
+                                        <label htmlFor="confirmPassword" className="form-label text-black">
+                                            Confirm Password
+                                        </label>
+                                        <input
+                                            type="password"
+                                            className="form-control"
+                                            id="confirmPassword"
+                                            name="confirmPassword"
+                                            value={confirmPassword}
+                                            onChange={handleChange}
+                                        />
+                                    </div>
+                                    <div className="d-grid gap-2">
+                                        <button type="submit" className="btn btn-dark text-white">
+                                            Change Password
+                                        </button>
+                                    </div>
+                                </form>
+                                {successMessage && (
+                                    <div className="text-success mt-3 text-black text-bold text-center">
+                                        {successMessage}
+                                    </div>
+                                )}
+                                {errorMessage && (
+                                    <div className="text-danger mt-3 text-black text-bold text-center">
+                                        {errorMessage}
+                                    </div>
+                                )}
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </section>
-    );
+            </section>
+        );
+    }
 }

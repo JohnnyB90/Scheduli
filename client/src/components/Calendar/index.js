94a730df22380ba7036c95eac4cb8ad9578ac3b3
Johnny <117334322+JohnnyB90@users.noreply.github.com>
2023-07-11 19:46:32 -0400

    Successfully created an array to hold all appointment data that will then be mapped over and rendered to the calendar

--- client/src/components/Calendar/index.js
+++ client/src/components/Calendar/index.js
@@ -1,13 +1,58 @@
-import React, { useRef, useEffect, useState } from 'react';
+// MyCalendar.js
+
+import React, { useEffect, useState } from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
+import { useQuery, useMutation } from '@apollo/client';
+import { gql } from 'graphql-tag';
+
 import './calendar.css';
 
-export default function MyCalendar() {
+const GET_APPOINTMENTS = gql`
+  query GetAppointments {
+    appointments {
+      _id
+      appointmentDate
+      appointmentTime
+      firstName
+      lastName
+    }
+  }
+`;
 
+const DELETE_APPOINTMENT = gql`
+  mutation DeleteAppointment($id: ID!) {
+    deleteAppointment(_id: $id) {
+      _id
+    }
+  }
+`;
+
+export default function MyCalendar() {
+  const { loading, error, data } = useQuery(GET_APPOINTMENTS);
+  const [deleteAppointment] = useMutation(DELETE_APPOINTMENT);
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    if (data) {
+      setEvents(data.appointments.map(a => ({
+        _id: a.id,
+        title: `${a.firstName} ${a.lastName}`,
+        start: `${a.appointmentDate}T${a.appointmentTime}`,
+      })));
+    }
+  }, [data]);
   
+  const handleEventClick = async ({ event }) => {
+    const shouldDelete = window.confirm('Are you sure you want to delete this event?');
+
+    if (shouldDelete) {
+      await deleteAppointment({ variables: { _id: event.id } });
+      setEvents(events.filter(e => e.id !== event.id));
+    }
+  };
 
   const calendarConfig = {
     plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
@@ -17,14 +62,18 @@ export default function MyCalendar() {
       center: 'title',
       right: 'dayGridMonth,timeGridWeek,timeGridDay',
     },
-    slotMinTime: '08:00:00', // Set the minimum time to 8 am
-    slotMaxTime: '21:00:00', // Set the maximum time to 8 pm
+    events,
+    eventClick: handleEventClick,
+    slotMinTime: '08:00:00',
+    slotMaxTime: '21:00:00',
   };
-  
+
+  if (loading) return 'Loading...';
+  if (error) return `Error! ${error.message}`;
 
   return (
     <div className="calendar-container">
-      <FullCalendar {...calendarConfig}/>
+      <FullCalendar {...calendarConfig} />
     </div>
   );
-};
+}

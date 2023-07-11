406bea6b361921856b592e14fa762d555a00c3cf
Johnny <117334322+JohnnyB90@users.noreply.github.com>
2023-07-10 21:43:47 -0400

    Attempting to correct heroku deployment error

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-require('dotenv').config({path: __dirname + '/../.env'});
+require('dotenv').config({ path: __dirname + '/../.env' });
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
@@ -11,32 +11,42 @@ const { resolvers } = require('./schemas');
 const db = require('./config/connection');
 
 const app = express();
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware,
 });
 
+// Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(routes);
 
-const PORT = process.env.PORT || 3001;
-
-const startApolloServer = async (typeDefs, resolvers) => {
+// Apollo Server
+async function startApolloServer() {
   await server.start();
   server.applyMiddleware({ app });
-  
-  db.once('open', () => {
+}
+
+// Routes
+app.use(routes);
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+}
+
+// Start the server
+const PORT = process.env.PORT || 3001;
+
+db.once('open', () => {
+  startApolloServer().then(() => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
       console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
     });
   });
-};
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
-startApolloServer(typeDefs, resolvers);
+});
